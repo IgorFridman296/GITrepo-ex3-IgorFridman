cd64ea43b9db13f8013698503c6d5a1082cb91ff
IgorFridman296 <fridman.igor.f@gmail.com>
2022-11-16 20:51:45 +0200

    New files

--- HelloIgor.java
+++ HelloIgor.java
@@ -1,7 +1,7 @@
 public class HelloIgor{
 	public static void main(String[] args){
 		for(int i=1; i<=10; i++){
-			System.out.println("Igor Fridman.");
+			System.out.println("Igor Fridman! Changed");
 		}
 	}
 }
